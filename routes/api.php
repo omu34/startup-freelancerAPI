14c8f655baa5c5b2640b7935fa416ede5d1746a2
Bernard Omuse <bernardomuse22@gmail.com>
2024-05-04 13:07:28 +0300

    w

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FreelancerController;
-use App\Http\Controllers\MediaController;
 use App\Http\Controllers\FileUploaderController;
-
-
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,7 +13,6 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
-
 
 Route::middleware(['auth:sanctum'])->group(function () {
     // User routes
@@ -27,13 +23,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     // Admin routes
     Route::get('/admin/pending-approval', [FreelancerController::class, 'adminFetchPendingApproval']);
     Route::put('/admin/profile/{profile}/approve', [FreelancerController::class, 'adminDecidesApproval']);
-
-
-
-
-// File Uploader
+    // File Uploader
     Route::post('/files', [FileUploaderController::class, 'store']);
     Route::get('/files/{id}/download', [FileUploaderController::class,'download']);
     Route::delete('/files/{id}/destroy', [FileUploaderController::class, 'destroy']);
-
 });
